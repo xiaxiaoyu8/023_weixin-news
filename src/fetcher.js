00f3fc2202af211bd6c99cb6b928a9b5e3377244
xiaxiaoyu8 <2739381256@qq.com>
2026-06-26 20:33:22 +0800

    4

--- src/fetcher.js
+++ src/fetcher.js
@@ -6,6 +6,16 @@ const parser = new RssParser({
   headers: { "User-Agent": "Mozilla/5.0 (compatible; NewsBot/1.0)" },
 });
 
+// const FEEDS = [
+//   { name: "BBC中文", url: "https://feeds.bbci.co.uk/zhongwen/simp/rss.xml", category: "国际" },
+//   { name: "36氪", url: "https://36kr.com/feed", category: "科技" },
+//   { name: "华尔街见闻", url: "https://wallstreetcn.com/feed", category: "财经" },
+//   { name: "纽约时报中文", url: "https://cn.nytimes.com/rss/", category: "国际" },
+//   { name: "FT中文网", url: "https://rsshub.app/ft/chinese/hot", category: "财经" },
+//   { name: "联合早报", url: "https://www.zaobao.com/news/china/rss.xml", category: "国内" },
+//   { name: "环球科学", url: "https://rsshub.app/huanqiukexue", category: "科技" },
+// ];
+
 const FEEDS = [
   { name: "BBC中文", url: "https://feeds.bbci.co.uk/zhongwen/simp/rss.xml", category: "国际" },
   { name: "36氪", url: "https://36kr.com/feed", category: "科技" },
@@ -14,6 +24,15 @@ const FEEDS = [
   { name: "FT中文网", url: "https://rsshub.app/ft/chinese/hot", category: "财经" },
   { name: "联合早报", url: "https://www.zaobao.com/news/china/rss.xml", category: "国内" },
   { name: "环球科学", url: "https://rsshub.app/huanqiukexue", category: "科技" },
+  { name: "财新网", url: "http://china.caixin.com/news/rss/100300241.xml", category: "国内" },
+  { name: "澎湃新闻", url: "https://rsshub.app/thepaper/featured", category: "国内" },
+  { name: "新京报", url: "http://www.bjnews.com.cn/feed", category: "国内" },
+  { name: "南方周末", url: "https://feedx.net/rss/infzm.xml", category: "国内" },
+  { name: "第一财经", url: "https://plink.anyfeeder.com/weixin/CBNweekly2008", category: "财经" },
+  { name: "21世纪经济报道", url: "https://plink.anyfeeder.com/weixin/jjbd21", category: "财经" },
+  { name: "界面新闻", url: "https://rsshub.app/jiemian", category: "财经" },
+  { name: "财经杂志", url: "http://www.caijing.com.cn/rss/110827686.xml", category: "财经" },
+  { name: "三联生活周刊", url: "http://feedmaker.kindle4rss.com/feeds/lifeweek.weixin.xml", category: "文化" },
 ];
 
 function isWithin24h(pubDate) {
